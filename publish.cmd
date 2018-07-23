3a4e8fbf910cfdcf76bcde1f51d46f481404e181
Jason Chou <jchou@microsoft.com>
2018-07-23 22:22:38 +0800

    Default EchoBot using Bot Builder SDK 4.0

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild AspNetCore-EchoBot-With-State.sln -p:DeployOnBuild=true -p:PublishProfile=jctestbot4-Web-Deploy.pubxml -p:Password=dLmlQtWDokG3ehkBK3s01cPFaKhcAKWoM3TCsFCR6nXk7Sbd0fHnLbdtMwLe
+
